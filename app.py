6f925d9bdd1defe8b06a76949f0771c5557c815a
Milan Mitrovic <milanmitrovic1991@gmail.com>
2022-04-30 21:00:55 +0200

    add create_personal_user page

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
 
-from dash import Dash, html, dcc, Input, Output, callback
+from dash import Dash, html, dcc, Input, Output
+from pages.personal_users import create_personal_user
 
 
-app = Dash(name=__name__, prevent_initial_callbacks=True)
+app = Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
 
 
 app.layout = html.Div([
-
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
+    html.Button(),
+    html.Div(id='page-content'),
+    dcc.Link('Go to Page 1', href='/create_personal_user')
 ])
 
 
-@callback(
+@app.callback(
     Output(component_id='page-content', component_property='children'),
     Input(component_id='url', component_property='pathname')
     )
 def display_page(pathname):
 
-    if pathname == 'create_personal_user':
+    if pathname == '/create_personal_user':
+        return create_personal_user.layout
+    elif pathname == '/delete_personal_user_account':
         return ''
-    elif pathname == 'delete_personal_user_account':
+    elif pathname == '/user_validation_number':
         return ''
-    elif pathname == 'user_validation_number':
+    elif pathname == '/read_personal_user':
         return ''
-    elif pathname == 'read_personal_user':
-        return ''
-    elif pathname == 'update_personal_user':
+    elif pathname == '/update_personal_user':
         return ''
 
 
